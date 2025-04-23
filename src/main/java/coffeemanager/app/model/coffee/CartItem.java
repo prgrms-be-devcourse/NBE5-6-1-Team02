455f5fda675d4bafc2cd34dd85e0bf62f1f2a0a9
[choidongjun] <[qkqehenr37@gmail.com]>
2025-04-23 16:27:47 +0900

    B feat: 주문 기능 구현

--- src/main/java/coffeemanager/app/model/coffee/CartItem.java
+++ src/main/java/coffeemanager/app/model/coffee/CartItem.java
@@ -1,6 +1,22 @@
 package coffeemanager.app.model.coffee;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CartItem {
 
+    private Long coffeeId;
+    private String name;
+    private int quantity;
+    private int price;
+
+    public CartItem(Long coffeeId, String name, int quantity, int price) {
+        this.coffeeId = coffeeId;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
 }
--- src/main/java/coffeemanager/app/repository/OrderRepository.java
+++ src/main/java/coffeemanager/app/repository/OrderRepository.java
@@ -1,5 +1,12 @@
 package coffeemanager.app.repository;
 
-public interface OrderRepository {
+import coffeemanager.app.model.coffee.Order;
+import org.apache.ibatis.annotations.Param;
 
+public interface OrderRepository {
+    void insertOrder(Order order);
+
+    void insertOrderCoffee(@Param("orderId") Long orderId,
+        @Param("coffeeId") Long coffeeId,
+        @Param("quantity") int quantity);
 }
