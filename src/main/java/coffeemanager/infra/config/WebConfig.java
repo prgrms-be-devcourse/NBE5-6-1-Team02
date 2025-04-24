6ea95dc6e0d2dd0d3b8407c6b440a933943cde76
hyeunS-P <hseric0816@gmail.com>
2025-04-24 17:44:32 +0900

    feat: 사진출력기능 추가

--- src/main/java/coffeemanager/infra/config/WebConfig.java
+++ src/main/java/coffeemanager/infra/config/WebConfig.java
@@ -1,15 +1,20 @@
 package coffeemanager.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
-            .addResourceLocations("file:///C:/Users/A/Desktop/firstProject/NBE5-6-1-Team02/upload/");
+            .addResourceLocations("file:" + uploadPath + "product/");
     }
 }
 
