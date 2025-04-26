e7f16fa40a69c80909f8d43500e2b41338c5fdb9
[choidongjun] <[qkqehenr37@gmail.com]>
2025-04-27 02:50:54 +0900

    refactor : DB Foreign Key 지정 및 테스트용 Dummy Data 추가

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -1,7 +1,25 @@
 insert into `COFFEE`
 
- (`COFFEE_ID`,`COFFEE_NAME`, `PRICE`)
-    VALUES (1000, 'ColumbiaNarino', 5000),
-           (1001, 'BrazilSerraDoCaparao', 5000),
-           (1003, 'ColumbiaQUindio', 5000),
-           (1004, 'EthiopiaSidamo', 5000);
+(`COFFEE_ID`, `COFFEE_IMG`, `COFFEE_NAME`, `PRICE`)
+VALUES (1001, 'product/ColumbiaNarino.jpeg', 'ColumbiaNarino', 5500),
+       (1002, 'product/BrazilSerraDoCaparao.jpg','BrazilSerraDoCaparao', 9400),
+       (1003, 'product/ColumbiaQUindio.jpg', 'ColumbiaQUindio', 10400),
+       (1004, 'product/EthiopiaSidamo.jpg','EthiopiaSidamo', 8800);
+
+INSERT INTO MEMBER (
+    USER_Email,
+    PASSWORD,
+    ROLE,
+    TEL,
+    ADDRESS,
+    ZIPCODE,
+    ACTIVATED
+) VALUES (
+             'super@aaa.aaa',
+             '{bcrypt}$2a$10$DLmC62iFtw6t/XTcLHhU/ONfwogNazrbae9PsnZ9DWz4/lIFr2jFK',
+             'ROLE_ADMIN',
+             '01022223333',
+             NULL,
+             NULL,
+             TRUE
+         );
--- src/main/resources/init/schema.sql
+++ src/main/resources/init/schema.sql
@@ -36,7 +36,9 @@ CREATE TABLE `ORDER` (
 -- 주문 상품 내역 테이블
 CREATE TABLE `ORDER_COFFEE` (
                                 `ORDER_NO` INT NOT NULL comment '주문번호',
-                                `COFFEE_ID` VARCHAR(100) NOT NULL comment '제품번호',
+                                `COFFEE_ID` INT NOT NULL comment '제품번호',
                                 `COFFEE_NAME` VARCHAR(200) NOT NULL comment '제품명',
-                                `QUANTITY` INT NOT NULL DEFAULT 0 comment '수량'
+                                `QUANTITY` INT NOT NULL DEFAULT 0 comment '수량',
+                                FOREIGN KEY (`ORDER_NO`) REFERENCES `ORDER` (`ORDER_NO`) ON DELETE CASCADE,
+                                FOREIGN KEY (`COFFEE_ID`) REFERENCES `COFFEE` (`COFFEE_ID`)
 );
